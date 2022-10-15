5f2a34e08d6a42487b46bfc7d1ff5c248cf39f2a
jasonLapina <lapina.jason@gmail.com>
2022-10-16 01:13:37 +0800

    finish header layout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { Fragment } from 'react';
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Header from './components/Layout/Header';
-
 function App() {
   return (
     <Fragment>
+      <Header />
       <Hero />
     </Fragment>
   );
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,13 @@
 import HeaderCart from './HeaderCart';
+import classes from './Header.module.scss';
+import logo from '../../assets/Logo.png';
 
 const Header = () => {
   return (
-    <header>
-      <a href='/'>nomnom LOGO</a>
+    <header className={classes.header}>
+      <a href='/'>
+        NomNom <img src={logo} alt='nomnom logo' />
+      </a>
       <div>deliver where</div>
       <div>deliver when</div>
       <div>
