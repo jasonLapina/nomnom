46e6ebca6ad4bb9bdfa41db0b4f55ab1a9a77a8c
jasonLapina <lapina.jason@gmail.com>
2022-10-17 01:22:18 +0800

    add cart badge animation

--- src/components/Layout/HeaderCart.js
+++ src/components/Layout/HeaderCart.js
@@ -1,17 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCart.module.scss';
 
 const HeaderCart = (props) => {
+  const [cartHasChanged, setCartHasChanged] = useState(false);
   const ctx = useContext(CartContext);
   const cartItems = ctx.items.map((item) => item.quantity);
   const length = cartItems.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
     0
   );
+  useEffect(() => {
+    if (length === 0) return;
+    // ENSURES ANIMATION WILL NOT PLAY BEFORE CURRENT ANIMATION ENDS //
+    if (cartHasChanged) return;
+    setCartHasChanged(true);
+    setTimeout(() => {
+      setCartHasChanged(false);
+    }, 1000);
+  }, [length]);
+
+  const cartClass = `${classes.cart} ${cartHasChanged ? classes.bump : ''}`;
 
   return (
-    <button onClick={props.onClick} className={classes.cart}>
+    <button onClick={props.onClick} className={cartClass}>
       <span>
         <ion-icon name='bag-handle-outline' />
       </span>
