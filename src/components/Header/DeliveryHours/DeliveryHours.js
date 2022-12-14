931847586213ad4a3c77d2f248debbf4847c17e5
jasonLapina <lapina.jason@gmail.com>
2022-12-14 09:03:40 +0800

    deliveryhours bugfix

--- src/components/Header/DeliveryHours/DeliveryHours.js
+++ src/components/Header/DeliveryHours/DeliveryHours.js
@@ -3,26 +3,29 @@ import classes from './DeliveryHours.module.scss';
 const DeliveryHours = () => {
   const curHour = new Date().getHours();
   // eslint-disable-next-line
-  const [isOpen, setIsOpen] = useState(curHour >= 5 && curHour < 21);
+  const isOpen = curHour >= 5 && curHour < 21;
   const [deliverTime, setDeliverTime] = useState('');
-
   const hours = Array.from({ length: 21 - curHour }).map(
     (_, i) => i + curHour + 1
   );
 
   useEffect(() => {
     if (isOpen) {
-      setDeliverTime(
-        `Now: (${
-          curHour > 11
-            ? `${curHour === 12 ? curHour : curHour - 12}PM`
-            : `${curHour}AM`
-        })`
-      );
+      const updatedDeliverTime =
+        curHour < 11 ? `${curHour}AM` : `${curHour + 12}PM`;
+      setDeliverTime(updatedDeliverTime);
     } else {
       setDeliverTime('Closed. Come back 5am to 9pm');
     }
   }, [curHour, isOpen]);
+
+  const renderAvailableHours = hours.map((hour, i) => {
+    return (
+      <option key={i} value={hour}>
+        {hour > 11 ? `${hour === 12 ? hour : hour - 12}PM` : `${hour}AM`}
+      </option>
+    );
+  });
 
   return (
     <select
@@ -31,17 +34,20 @@ const DeliveryHours = () => {
       name='deliverTime'
       id='deliverTime'
     >
-      <option value='now'>{deliverTime}</option>
-      {isOpen &&
-        hours.map((hour, i) => {
-          return (
-            <option key={i} value={hour}>
-              {hour > 11 ? `${hour === 12 ? hour : hour - 12}PM` : `${hour}AM`}
-            </option>
-          );
-        })}
+      <option value='now'>{`Now (${hours[0] - 1}${
+        curHour < 11 ? 'AM' : 'PM'
+      })`}</option>
+      {isOpen && renderAvailableHours}
     </select>
   );
 };
 
 export default DeliveryHours;
+
+/*
+ `Now: (${
+          curHour > 11
+            ? `${curHour === 12 ? curHour : curHour - 12}PM`
+            : `${curHour}AM`
+        })`
+*/
